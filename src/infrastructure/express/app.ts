203cfcfa69545fde93c0bc85a5de7aa790fdce2d
David Topping <topping.david11@gmail.com>
2026-01-11 12:55:53 +0000

    feat: adds base path env var

--- src/infrastructure/express/app.ts
+++ src/infrastructure/express/app.ts
@@ -3,6 +3,9 @@ import { registerRoutes } from "./routes";
 
 export const app = express();
 
+const API_BASE_PATH = process.env.API_BASE_PATH || "";
+console.log(API_BASE_PATH);
 app.use(express.json());
-
-registerRoutes(app);
+const router = express.Router();
+registerRoutes(router);
+app.use(API_BASE_PATH, router);
